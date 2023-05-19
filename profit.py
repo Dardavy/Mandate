3b7bd05bf6b0718cecb3618a0120831e6d8e6485
Dardavy <127051857+Dardavy@users.noreply.github.com>
2023-05-19 23:05:24 +0100

    Update profit.py

--- profit.py
+++ profit.py
@@ -1,29 +1,43 @@
+
+
+
+
+
+
+
+
+
+      
+                  
+
+
+
+                
 import time
 import schedule
 
-Amount_investment = float(input("Enter your Amount investment: "))
-profit = Amount_investment * 0.1
+initial_balance = float(input("Enter your initial balance: "))
+profit_rate = 0.1  # 10% profit rate
 
+def calculate_investment_balance():
+    global initial_balance, profit_rate
+    investment_balance = initial_balance + (initial_balance * profit_rate)
+    profit = investment_balance * profit_rate
+    initial_balance = investment_balance
+    print("Investment balance for the next day:", investment_balance)
+    print("Profit for the next day:", profit)
 
-def geT_Profit(Amount_investment, profit):
-        geT_Profit = Amount_investment + profit
-        return geT_Profit
+schedule.every().day.at("00:00").do(calculate_investment_balance)
 
-
-def calc():
-        if __name__ == "__main__" :
-                Amount_investment,profit
-
-        if Amount_investment < 0 :
-                print("no profit gain")
-
-        elif Amount_investment > 0: 
-                print(geT_Profit(Amount_investment,profit), "profit") 
-                  
-calc()               
-schedule.every().day.at("00:00").do(calc)
+def calculate_investment_balance()
 
 while True:
-        schedule.run_pending()
-        time.sleep
+    schedule.run_pending()
+    time.sleep(1)
+              
+                
+                
+                
+                
+                
                 
