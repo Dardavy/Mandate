495c19fc895f4c704812309588711d3623178e73
Dardavy <127051857+Dardavy@users.noreply.github.com>
2023-05-20 13:26:26 +0100

    Update profit.py

--- profit.py
+++ profit.py
@@ -10,8 +10,8 @@ def calculate_investment_balance():
     investment_balance = initial_balance + (initial_balance * profit_rate)
     profit = investment_balance * profit_rate
     initial_balance = investment_balance
-    print("Investment balance for the next day:", investment_balance)
-    print("Profit for the next day:", profit)
+    print(f"Investment balance for the next day is: {investment_balance: .2f}")
+    print(f"Profit for the next day is: {profit: .2f}")
 
 schedule.every().day.at("00:00").do(calculate_investment_balance)
 
